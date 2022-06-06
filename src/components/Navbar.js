da8e2778e5ffc16b72389a61ab0f3aab893ad132
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-06-06 11:28:48 +0300

    Add back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Navbar() {
+  const history = useHistory();
+
   return (
     <div className="chat-navbar">
       <nav className="chat-navbar-inner">
         <div className="chat-navbar-inner-left">
+          <button
+            onClick={() => history.goBack()}
+            className="btn btn-outline-primary"
+          >
+            Back
+          </button>
           <Link to="/settings" className="btn btn-outline-success ml-2">
             Settings
           </Link>
